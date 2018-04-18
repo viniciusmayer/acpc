8682fba5ce17168960ef27aeafec59a910098fba
Vinicius Mayer <viniciusmayer@gmail.com>
2018-04-18 14:17:58 -0300

    Inclusão dos trabalhos em banco de dados

--- acpa/LattesService.py
+++ acpa/LattesService.py
@@ -1,8 +1,13 @@
 from acpa.Entity import Trabalho
+import sqlite3
 
 class Processar(object):
     
     def __init__(self, arquivoXML):
+        self.conn = sqlite3.connect('lattes.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('create table if not exists trabalho(id INTEGER PRIMARY KEY, titulo TEXT, ano INTEGER, natureza TEXT)')
+        self.conn.commit()
         self.arquivoXML = arquivoXML
         self.trabalhos = []
         self.contador = 0
@@ -18,10 +23,20 @@ class Processar(object):
                 detalhamento = elemento.getElementsByTagName(detalhamentoTagName)
                 titulo = detalhamento[0].getAttribute(nomeDoEventoAttributeName)
             self.trabalhos.append(Trabalho(self.proximoNumero(), ano, titulo, natureza))
+            insert = 'insert into trabalho (titulo, ano, natureza) values ({0}, {1}, {2})'
+            insert = insert.format(titulo, ano, natureza)
+            self.cursor.execute(insert)
+            self.cursor.commit()
 
     def imprimir(self):
         for trabalho in self.trabalhos:
             trabalho.imprimir()
+            
+    def imprimirDb(self):
+        self.cursor.execute('select id, titulo, ano, natureza from trabalho')
+        for linha in self.cursor.fetchall():
+            t = Trabalho(linha[0], linha[2], linha[1], linha[3])
+            t.imprimir()
             
     def proximoNumero(self):
         self.contador += 1
