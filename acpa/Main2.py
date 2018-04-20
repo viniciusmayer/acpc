2059a26987f0461f45d20d60abb98b355f4528f4
Vinicius Mayer <viniciusmayer@gmail.com>
2018-04-20 18:52:04 -0300

    Utilização de dicionario para armazenar o nome do arquivo/arquivo e inclusão apenas de arquivos lidos e número de pagínas obtido

--- acpa/Main2.py
+++ acpa/Main2.py
@@ -2,13 +2,12 @@ from acpa.PDFService import PDFService
 
 if __name__ == '__main__':
     arquivoCabecalho = '../files/cabecalho.pdf'
-    pastaPDFs = '/home/eleonorvinicius/Documents/'
+    pastaPDFs = '/Users/eleonor.mayer/Documents/'
     arquivoFinal = '../files/destino.pdf'
     
-    print('===> lendo arquivos')
-    service = PDFService(pastaPDFs, arquivoFinal)
-    print('===> gerando cabecalho')
-    numeroPaginas = service.numeroPaginas()
-    service.gerarCabecalho(arquivoCabecalho, numeroPaginas)
-    print('===> gerando arquivo')
-    service.gerarArquivo(arquivoCabecalho, numeroPaginas)
+    print('===> reading files')
+    service = PDFService(pastaPDFs)
+    print('===> writing header')
+    service.gerarCabecalho(arquivoCabecalho)
+    print('===> writing file')
+    service.gerarArquivo(arquivoCabecalho, arquivoFinal)
