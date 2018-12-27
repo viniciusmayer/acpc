53cb654d5c53641571a4c0705a0662a0fc8941b4
Vinicius Mayer <viniciusmayer@gmail.com>
2018-12-27 17:36:40 -0200

    Inclusao do atributo ativo nos inserts

--- acpc/GerarEventoService.py
+++ acpc/GerarEventoService.py
@@ -8,14 +8,14 @@ class GerarEventoService(object):
         self.cursor = self.connection.cursor()
     
     def processar(self, nome, quando):
-        insertEvento = 'INSERT INTO public.trabalhos_evento(nome, quando) VALUES (\'{0}\', \'{1}\')'.format(nome, quando.strftime('%Y-%m-%d'))
+        insertEvento = 'INSERT INTO public.trabalhos_evento(nome, quando, ativo) VALUES (\'{0}\', \'{1}\', true)'.format(nome, quando.strftime('%Y-%m-%d'))
         self.cursor.execute(insertEvento)
         self.connection.commit()
         print('evento inserido: {0}, {1}'.format(nome, quando.strftime('%Y-%m-%d')))
                 
         selectEvento = 'SELECT id FROM public.trabalhos_evento where nome = \'{0}\' and quando = \'{1}\''.format(nome, quando.strftime('%Y-%m-%d'))
-        insertEventoTrabalho = 'INSERT INTO public.trabalhos_eventotrabalho(ordem, evento_id, trabalho_id) \
-                                    VALUES ({0}, ({1}), {2})'
+        insertEventoTrabalho = 'INSERT INTO public.trabalhos_eventotrabalho(ordem, evento_id, trabalho_id, ativo) \
+                                    VALUES ({0}, ({1}), {2}, true)'
         selectTrabalhos = 'select t.id from public.trabalhos_trabalho t \
                                 inner join public.trabalhos_tag ta on ta.id=t.tag_id \
                             order by ta.ordem asc, t.ano desc, t.ano_fim desc, t.titulo asc'
